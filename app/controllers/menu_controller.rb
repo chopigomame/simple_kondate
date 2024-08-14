2b4ecbec3e42601faf1c15f2a216f9cb3f837c37
higa <aaa@aaa.com>
2024-08-14 15:42:58 +0900

    Remove un-necessary comments

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,7 +1,6 @@
 class MenuController < ActionController::Base
 	protect_from_forgery # https://techracho.bpsinc.jp/hachi8833/2021_11_26/46891
 	def show
-		# TODO: リクエストを受け取って適切なメニューを選択してきて、Viewに渡す処理を書く必要がある
 		@cuisine = params[:cuisine]
 		@main_ingredient = params[:main_ingredient]
 		@is_soup = params[:soup] == "汁物"
